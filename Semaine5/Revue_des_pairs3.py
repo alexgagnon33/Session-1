f54a00c1a0cbbe0b75abc76895ae0e65e9b359fa
alexgagnon33 <116171801+alexgagnon33@users.noreply.github.com>
2022-11-18 10:21:28 -0500

    dsdsdsd

---
+++ Semaine5/Revue_des_pairs3.py
@@ -0,0 +1,75 @@
+#créer un fichier/le nommer
+#nb rdm dans le fichier seulement positif
+#puis suivre les règles
+
+def document_nb():
+    nom_fichier = int(input('Écriver le nom du document'))
+    nb_choisi = int(input('Écriver des nombres a inclure dans le document'))
+    nb_croissant = sorted(nb_choisi) 
+    nb_decroissant = nb_choisi.sort (Fasle=True)
+    nb_max = max(nb_choisi)
+    nb_min = min(nb_choisi)
+    nb_moy = sum(nb_choisi)/len(nb_choisi)
+    nb_med = dsdsds
+    nb_mode = 
+if nb_choisi >= 0:
+
+
+
+def doc_nb():
+    nom_fichier = int(input('Écriver le nom du document'))
+    nb_choisi = int(input('Écriver des nombres a inclure dans le document'))
+    document = {
+    'nb_croissant' : nb_choisi.sort() 
+    'nb_decroissant' : nb_choisi.sort (Fasle=True)
+}
+
+
+
+
+
+
+def doc_nb():
+    nom_fichier = int(input('Écriver le nom du document'))
+    nb_choisi = int(input('Écriver des nombres a inclure dans le document'))
+    nb_croissant = nb_choisi.sort() 
+    nb_decroissant = nb_choisi.sort (Fasle=True)
+    nb_max = max(nb_choisi)
+    nb_min = min(nb_choisi)
+    nb_moy = sum(nb_choisi)/len(nb_choisi)
+def median(l):
+    half = len(l) // 2
+    l.sort()
+    if not len(l) % 2:
+        return (l[half - 1] + l[half]) / 2.0
+    return l[half]
+if nb_choisi >= 0:
+    with open('{nom_fichier}.txt', 'x') as f:
+        f.write{nb_croissant}
+else:
+    print('Vos nombres choisi sont négatif')
+
+
+
+
+def document():
+    nb_document = int(input('Écriver les nombre que vous voulez écrire dans le document avec un espace'))
+    print("\n")
+    user_list = nb_document.split()
+    print('list: ', user_list)
+    for i in range(len(user_list)):
+        user_list[i] = int(user_list[i])
+
+#Donc doit définir et faire des boucles pour trouver tous les éléments de la liste
+#puis return à la fin de la boucle
+
+def plus_petit(données )
+
+def ordre_croissant(données):
+    triés = [données]
+    while len (données) > 0:
+    min = plus_petit(données)
+    données.remove(min)
+    données.append(min)
+
+    for in 
