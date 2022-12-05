eb35cd4148081edc7124887f25c5891252f52a3f
Alexandre Gagnon <116171801+alexgagnon33@users.noreply.github.com>
2022-12-05 10:39:06 -0500

    dasdas

--- Semaine5/Revue_des_pairs3.py
+++ Semaine5/Revue_des_pairs3.py
@@ -73,3 +73,25 @@ def ordre_croissant(données):
     données.append(min)
 
     for in 
+    
+"""
+Exercice 5 Revue des pairs:
+Offrir à l'utilisateur d'entrer un nom de fichier et un nombre illimité de nombres positifs, tant et aussi longtemps qu'il ne rentre 
+pas un nombre négatif. Ajouter les nombres entrés par l'utilisateur à une liste en excluant le nombre négatif. Ensuite, écrire les résultats
+suivants dans le fichier nommé par l'utilisateur:
+
+La liste en ordre croissant
+La liste en ordre décroissant
+Le maximum
+Le minimum
+La moyenne de la liste
+La médiane (la valeur à la position centrale si la longueur de la liste est impaire et la moyenne des deux valeurs centrales si paire)
+Ex: 1, 2, 3, 5, 4. Médiane = 3.
+Ex: 1, 2, 3, 4, 5, 6. Médiane = 3.5
+Le mode (l'occurrence la plus fréquente s'il y a lieu. Si chaque entrée est unique, inscrire que le mode = none)
+Ex: 1, 2, 2, 2, 3, 4, 3, 4. La mode = 2
+
+Vous ne pouvez pas utiliser de module, vous devez donc vous-même implémenter l'algorithme pour trier une liste en ordre croissant, trier une 
+liste en ordre décroissant, trouver le maximum, trouver le minimum, calculer la moyenne, trouver la médiane, et trouver le mode
+(utilisez un dictionnaire pour chaque donnée en tant que clef, affectez 0 comme valeur initiale et incrémentez de 1 à chaque répétition).
+"""
---
+++ Semaine6/Revue_des_pairs_4.py
@@ -0,0 +1,23 @@
+"""
+Partie 1:
+Créer un dictionnaire possédant les cours que vous suivez cette session et leur enseignant respectif. Par exemple:
+Keven Presseau-St-Laurent - Concepts de programmation 1
+
+Ensuite, afficher un menu à la console présentant les 3 cours et offrant à l'utilisateur d'en sélectionner 1. 
+Lorsque l'utilisateur à fait sa sélection, afficher le nom de l'enseignant et le nom du cours à l'écran.
+
+Partie 2:
+En se basant sur la partie 1, créer un fichier bdd.txt fonctionnant comme une base de données et remplir le dictionnaire à partir 
+de ce fichier. Pour vous faciliter la tâche, vous pouvez écrire les informations de la manière suivante:
+Nom de cours 1
+Nom de prof 1
+Nom de cours 2
+
+Partie 3:
+En se basant sur la partie 2, modifier le menu utilisateur en y ajoutant une option lui permettant de faire une recherche d'enseignant. 
+Vérifier si l'enseignant entré par l'utilisateur est présent dans votre liste de cours et indiquer le résultat à la console.
+
+Partie 4:
+Offrir à l'utilisateur une nouvelle option au menu lui permettant d'ajouter un cours et un nom d'enseignant à la base de données de 
+la partie 2. Une fois les données utilisateurs entrées, ajouter les informations à la fin du document bdd.txt
+"""
