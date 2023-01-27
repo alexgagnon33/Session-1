54b03bd1fc1572b8bc924eb64601a55ea8f57aa1
Alexandre Gagnon <116171801+alexgagnon33@users.noreply.github.com>
2023-01-27 04:36:08 -0500

    dsds

--- Semaine6/Revvvv.py
+++ Semaine6/Revvvv.py
@@ -1,24 +1,33 @@
+#Partie 1
+
 cours = {
-    "CP1": "Keven Presseau-St-Laurent",
-    "CP2": "John Doe",
-    "CP3": "Jane Smith"
+    "Concepts de programmation 1": "Keven Presseau-St-Laurent",
+    "Systèmes d'exploitation": "Keven Presseau-St-Laurent",
+    "Logique mathématique pour les professionnels de l'informatique": "Nicolas Thavonekham Robidoux"
 }
 
+#Partie 3
+
 while True:
+
     print("Sélectionnez une option:")
     print("1. Sélectionnez un cours")
     print("2. Recherchez un enseignant")
     print("3. Ajouter un cours et un enseignant")
     print("4. Quitter")
+
     choix = int(input())
+
     if choix == 1:
         print("Sélectionnez un cours:")
+
         for i, c in enumerate(cours):
             print(f"{i + 1}. {c}")
         cours_choix = int(input())
         nom_cours = list(cours.keys())[cours_choix - 1]
         nom_enseignant = cours[nom_cours]
         print(f"L'enseignant pour {nom_cours} est {nom_enseignant}.")
+
     elif choix == 2:
         print("Sélectionnez un enseignant:")
         for i, e in enumerate(cours.values()):
@@ -29,14 +38,17 @@ while True:
                 nom_cours = c
                 nom_enseignant = e
                 break
+
         print(f"L'enseignant {nom_enseignant} enseigne le cours {nom_cours}.")
+
     elif choix == 3:
         nom_cours = input("Entrez le nom du cours: ")
         nom_enseignant = input("Entrez le nom de l'enseignant: ")
         cours[nom_cours] = nom_enseignant
         print("Cours et enseignant ajoutés.")
-        with open("Revue4\pomme.txt", "a", encoding="UTF-8") as file:
+        with open("Semaine6\pomme.txt", "a", encoding="UTF-8") as file:
             file.write(str(cours))
+
     elif choix == 4:
         break
     else:
