343cc610750a9f161f6164eb9d2c392b50eb03a6
Alexandre Gagnon <116171801+alexgagnon33@users.noreply.github.com>
2023-01-27 04:51:21 -0500

    Update Revue_des_pairs_5.py

--- Session10/Revue_des_pairs_5.py
+++ Session10/Revue_des_pairs_5.py
@@ -34,6 +34,47 @@ class Fraction:
     def __str__(self):
         return f'{self.numerator}/{self.denominator}'
 
+def menu():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Compare")
+    print("4. Multiply")
+    print("5. Divide")
+    print("6. Raise to a power")
+    choice = input("Enter choice (1/2/3/4/5/6):")
+
+    if choice == '1':
+        num1 = Fraction(int(input("Enter numerator for first fraction: ")), int(input("Enter denominator for first fraction: ")))
+        num2 = Fraction(int(input("Enter numerator for second fraction: ")), int(input("Enter denominator for second fraction: ")))
+        print(num1 + num2)
+    elif choice == '2':
+        num1 = Fraction(int(input("Enter numerator for first fraction: ")), int(input("Enter denominator for first fraction: ")))
+        num2 = Fraction(int(input("Enter numerator for second fraction: ")), int(input("Enter denominator for second fraction: ")))
+        print(num1 - num2)
+    elif choice == '3':
+        num1 = Fraction(int(input("Enter numerator for first fraction: ")), int(input("Enter denominator for first fraction: ")))
+        num2 = Fraction(int(input("Enter numerator for second fraction: ")), int(input("Enter denominator for second fraction: ")))
+        if num1 == num2:
+            print("The fractions are equal")
+        else:
+            print("The fractions are not equal")
+    elif choice == '4':
+        num1 = Fraction(int(input("Enter numerator for first fraction: ")), int(input("Enter denominator for first fraction: ")))
+        num2 = Fraction(int(input("Enter numerator for second fraction: ")), int(input("Enter denominator for second fraction: ")))
+        print(num1 * num2)
+    elif choice == '5':
+        num1 = Fraction(int(input("Enter numerator for first fraction: ")), int(input("Enter denominator for first fraction: ")))
+        num2 = Fraction(int(input("Enter numerator for second fraction: ")), int(input("Enter denominator for second fraction: ")))
+        print(num1 / num2)
+    elif choice == '6':
+        num1 = Fraction(int(input("Enter numerator for fraction: ")), int(input("Enter denominator for fraction: ")))
+        power = int(input("Enter power to raise the fraction to: "))
+        print(num1 ** power)
+    else:
+        print("Invalid input")
+
+
 fraction1 = Fraction(1, 2)
 fraction2 = Fraction(2, 3)
 print(fraction1 + fraction2) # 3/6
