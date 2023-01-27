985435779f2a85a211cdf7c80587dbae907e73ce
Alexandre Gagnon <116171801+alexgagnon33@users.noreply.github.com>
2023-01-27 04:11:23 -0500

    Update Revvvv.py

--- Semaine6/Revvvv.py
+++ Semaine6/Revvvv.py
@@ -4,12 +4,34 @@ cours = {
     "CP3": "Jane Smith"
 }
 
-print("Sélectionnez un cours:")
-for i, c in enumerate(cours):
-    print(f"{i + 1}. {c}")
+while True:
+    print("Sélectionnez une option:")
+    print("1. Sélectionnez un cours")
+    print("2. Recherchez un enseignant")
+    print("3. Quitter")
 
-choix = int(input())
-nom_cours = list(cours.keys())[choix - 1]
-nom_enseignant = cours[nom_cours]
 
-print(f"L'enseignant pour {nom_cours} est {nom_enseignant}.")
+    choix = int(input())
+    if choix == 1:
+        print("Sélectionnez un cours:")
+        for i, c in enumerate(cours):
+            print(f"{i + 1}. {c}")
+            cours_choix = int(input())
+            nom_cours = list(cours.keys())[cours_choix - 1]
+            nom_enseignant = cours[nom_cours]
+            print(f"L'enseignant pour {nom_cours} est {nom_enseignant}.")
+    elif choix == 2:
+        print("Sélectionnez un enseignant:")
+        for i, e in enumerate(cours.values()):
+            print(f"{i + 1}. {e}")
+            enseignant_choix = int(input())
+            nom_enseignant = list(cours.values())[enseignant_choix - 1]
+        for c, e in cours.items():
+            if e == nom_enseignant:
+                nom_cours = c
+                break
+        print(f"L'enseignant {nom_enseignant} enseigne le cours {nom_cours}.")
+    elif choix == 3:
+        break
+    else:
+        print("Choix non valide, veuillez réessayer.")
