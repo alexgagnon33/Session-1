1d31fe49a4e05b12378f2def6f65d122996dc11b
Alexandre Gagnon <116171801+alexgagnon33@users.noreply.github.com>
2023-01-27 03:24:51 -0500

    Create Revue4.py

---
+++ Semaine6/Revue4.py
@@ -0,0 +1,49 @@
+#Partie1
+
+courses = {}
+with open("Semaine6\Prof.txt", "r", encoding="UTF-8") as prof_file:
+    lines = prof_file.readlines()
+    for i in range(0, len(lines), 2):
+        course_name = lines[i].strip()
+        teacher_name = lines[i+1].strip()
+        courses[course_name] = teacher_name
+
+#Partie 3
+while True:
+    print("1. Sélectionner un cours")
+    print("2. Rechercher un enseignant")
+    print("3. Ajouter un cours")
+    print("4. Quitter")
+    choice = int(input())
+
+
+
+    if choice == 1:
+        print("Sélectionnez un cours:")
+        for i, course in enumerate(courses):
+            print(f"{i+1}. {course}")
+
+        selected_course = int(input())
+
+        course_name = list(courses.keys())[selected_course - 1]
+        teacher_name = courses[course_name]
+        print(f"Enseignant: {teacher_name}\nCours: {course_name}")
+    
+    elif choice == 2:
+        teacher_name = input("Entrez le nom de l'enseignant à rechercher: ")
+        found = False
+        for course, teacher in courses.items():
+            if teacher.lower() == teacher_name.lower():
+                print(f"{teacher} enseigne le cours {course}")
+                found = True
+            if not found:
+                print(f"Aucun cours enseigné par {teacher_name} n'a été trouvé.")
+        
+    elif choice == 3:
+        course_name = input("Entrez le nom du cours à ajouter: ")
+        teacher_name = input("Entrez le nom de l'enseignant: ")
+        courses[course_name] = teacher_name
+    elif choice == 4:
+        break
+    else:
+        print("Sélection non valide. Veuillez sélectionner une option valide.")
